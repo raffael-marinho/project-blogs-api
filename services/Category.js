491dd57b831f97e4c847eb5b8a50c07bffdff9da
raffael marinho <silvamarinho10@gmail.com>
2022-05-10 06:55:44 -0300

    fix:depois eu organizo

--- services/Category.js
+++ services/Category.js
@@ -1,19 +1,19 @@
-const Category = require('../models/Category');
+// const Category = require('../models/Category');
 
-async function addCategory(name) {
-  if (!name) {
-    throw new Error('"name" is required');
-  }
-  const category = await Category.create({ name });
-  return category;
-}
+// async function addCategory(name) {
+//   if (!name) {
+//     throw new Error('"name" is required');
+//   }
+//   const category = await Category.create({ name });
+//   return category;
+// }
 
-async function getAllCategories() {
-  const categories = await Category.findAll();
-  return categories;
-}
+// async function getAllCategories() {
+//   const categories = await Category.findAll();
+//   return categories;
+// }
 
-module.exports = {
-  addCategory,
-  getAllCategories,
-};
+// module.exports = {
+//   addCategory,
+//   getAllCategories,
+// };
--- services/User.js
+++ services/User.js
@@ -1,42 +1,42 @@
-const User = require('../models/User');
-const { tokenGenerator } = require('../token/token');
+// const User = require('../models/User');
+// const { tokenGenerator } = require('../token/token');
 
-async function addUser(displayName, email, password, image) {
-  const emailVerify = await User.findOne({ where: { email } });
-  console.log(emailVerify);
-  if (emailVerify) {
-    throw new Error('User already registered');
-  }
-  const user = await User.create({ displayName, email, password, image });
+// async function addUser(displayName, email, password, image) {
+//   const emailVerify = await User.findOne({ where: { email } });
+//   console.log(emailVerify);
+//   if (emailVerify) {
+//     throw new Error('User already registered');
+//   }
+//   const user = await User.create({ displayName, email, password, image });
 
-  return user;
-}
+//   return user;
+// }
 
-async function login(email, password) {
-  const user = await User.findOne({ where: { email } });
-  if (!user || password !== user.password) {
-    throw new Error('Invalid fields');
-  }
-  const token = tokenGenerator(user.id);
-  return token;
-}
+// async function login(email, password) {
+//   const user = await User.findOne({ where: { email } });
+//   if (!user || password !== user.password) {
+//     throw new Error('Invalid fields');
+//   }
+//   const token = tokenGenerator(user.id);
+//   return token;
+// }
 
-async function getAllUsers() {
-  const users = await User.findAll();
-  return users;
-}
+// async function getAllUsers() {
+//   const users = await User.findAll();
+//   return users;
+// }
 
-async function getUserById(id) {
-  const user = await User.findByPk(id);
-  if (!user) {
-    throw new Error('User does not exist');
-  }
-  return user;
-}
+// async function getUserById(id) {
+//   const user = await User.findByPk(id);
+//   if (!user) {
+//     throw new Error('User does not exist');
+//   }
+//   return user;
+// }
 
-module.exports = {
-  addUser,
-  login,
-  getAllUsers,
-  getUserById,
-};
+// module.exports = {
+//   addUser,
+//   login,
+//   getAllUsers,
+//   getUserById,
+// };
